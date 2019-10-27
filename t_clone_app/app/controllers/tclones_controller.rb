330400b05dd3f614e0dfdd316520ae5105ae6884
Wataru Hikichi <w.hikichi.kg@gmail.com>
2019-10-27 20:19:39 +0900

    サンプル

--- t_clone_app/app/controllers/tclones_controller.rb
+++ t_clone_app/app/controllers/tclones_controller.rb
@@ -2,5 +2,13 @@ class TclonesController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @Tweet = Tweet.new
+  end
+
+  def create
+    @Tweet = Tweet.create(content: params[:tweet][:content])
+  end
+
 end
--- t_clone_app/config/routes.rb
+++ t_clone_app/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'tclones/index'
-  resources :tweets
+  resources :tclones
 end

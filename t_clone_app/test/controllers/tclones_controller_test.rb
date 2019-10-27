433b476f58e7eaf0443d271c709b02cafa990f96
Wataru Hikichi <w.hikichi.kg@gmail.com>
2019-10-27 20:29:53 +0900

    アプリ削除

--- t_clone_app/test/controllers/tclones_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TclonesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tclones_index_url
-    assert_response :success
-  end
-
-end

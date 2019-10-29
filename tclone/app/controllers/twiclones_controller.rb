fd0c69825292d1efb09e968ca684254171c5b7e3
Wataru Hikichi <w.hikichi.kg@gmail.com>
2019-10-29 19:56:57 +0900

    DBへ本文登録

--- tclone/app/controllers/twiclones_controller.rb
+++ tclone/app/controllers/twiclones_controller.rb
@@ -4,6 +4,18 @@ class TwiclonesController < ApplicationController
   end
 
   def new
+    @twiclone = Twiclone.new
+  end
+
+  def create
+    Twiclone.create(twiclone_params)
+    redirect_to new_twiclone_path #リダイレクトを行う
+  end
+
+  private
+
+  def twiclone_params
+    params.require(:twiclone).permit(:content)
   end
 
 end
